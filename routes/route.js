231ca8ba3bf9c4bd5d3265eabc25e9628530a5ed
jimmatthew <jameslindstrom2@gmail.com>
2024-09-18 19:41:42 +0000

    initial sftp support

--- routes/route.js
+++ routes/route.js
@@ -145,5 +145,83 @@ module.exports = function (uploadsDir, isAuthenticated) {
       }
     })
   })
+
+  const SftpClient = require('ssh2-sftp-client');
+
+  router.get('/sftp', (req, res) => {
+    res.render('sftp', { files: null, message: null });
+  });
+  
+  // Handle SFTP connection and browsing
+  router.post('/sftp/connect', async (req, res) => {
+    const { host, username, password, directory } = req.body;
+    const sftp = new SftpClient();
+    const currentDirectory = directory || '/'; // Default to root if not provided
+  
+    try {
+      await sftp.connect({ host, username, password });
+      const fileList = await sftp.list(currentDirectory);
+      res.render('sftplist', { 
+        files: fileList, 
+        message: `Connected to SFTP server at ${currentDirectory}`, 
+        currentDirectory, 
+        host, 
+        username, 
+        password 
+      });
+      await sftp.end();
+    } catch (err) {
+      console.error(err);
+      res.render('sftplist', { 
+        files: null, 
+        message: 'Failed to connect or access the directory', 
+        currentDirectory, 
+        host, 
+        username, 
+        password 
+      });
+    }
+  });
+
+  router.post('/sftp/upload', async (req, res) => {
+    const { host, username, password, localFilePath, remoteFilePath } = req.body;
+    const sftp = new SftpClient();
+    
+    try {
+      await sftp.connect({ host, username, password });
+      await sftp.fastPut(localFilePath, remoteFilePath); // Upload local file to remote path
+      res.render('sftp', { files: null, message: 'File uploaded successfully' });
+      await sftp.end();
+    } catch (err) {
+      console.error(err);
+      res.render('sftp', { files: null, message: 'File upload failed' });
+    }
+  });
+
+  router.post('/sftp/download', async (req, res) => {
+    const { host, username, password, remoteFilePath, localFilePath } = req.body;
+    const sftp = new SftpClient();
+    
+    try {
+      await sftp.connect({ host, username, password });
+      await sftp.fastGet(remoteFilePath, uploadsDir+'/temp'); // Download remote file to local path
+      const filePath = path.join(uploadsDir, 'temp')
+      res.download(filePath, (err) => {
+      if (err) {
+        return res.status(500).send('File not found')
+      }
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          return res.status(500).send('unable to delete file')
+        }
+      })
+    })
+      //res.render('sftp', { files: null, message: 'File downloaded successfully' });
+      await sftp.end();
+    } catch (err) {
+      console.error(err);
+      res.render('sftp', { files: null, message: 'File download failed' });
+    }
+  });
   return router
 }
