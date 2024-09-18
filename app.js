5ddd9595895dbeed0dffb02c2ea6f959b61ed929
jimmatthew <jameslindstrom2@gmail.com>
2024-09-18 18:33:37 +0000

    cleanup

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
 const bcrypt = require('bcryptjs');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const users = [
   { id: 1, username: 'admin', passwordHash: bcrypt.hashSync('123', 10) } 
 ]
 
-var app = express();
+const app = express()
 
 app.use(session({
   secret: 'secret_key',
@@ -23,52 +23,52 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 passport.use(new LocalStrategy(
   (username, password, done) => {
-    const user = users.find(user => user.username === username);
+    const user = users.find(user => user.username === username)
     if (!user) {
-      return done(null, false, { message: 'Incorrect username.' });
+      return done(null, false, { message: 'Incorrect username.' })
     }
     if (!bcrypt.compareSync(password, user.passwordHash)) {
-      return done(null, false, { message: 'Incorrect password.' });
+      return done(null, false, { message: 'Incorrect password.' })
     }
-    return done(null, user);
+    return done(null, user)
   }
-));
+))
 
 // Serialize and deserialize user
 passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+  done(null, user.id)
+})
 
 passport.deserializeUser((id, done) => {
-  const user = users.find(user => user.id === id);
-  done(null, user);
-});
+  const user = users.find(user => user.id === id)
+  done(null, user)
+})
 
 // Middleware to check if the user is authenticated
 function isAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    return next();
+    return next()
   }
-  res.redirect('/login');
+  res.redirect('/login')
 }
 
-const routes = require('./routes/route')(path.join(__dirname, 'uploads'), isAuthenticated);
+const routes = require('./routes/route')(path.join(__dirname, 'uploads'), isAuthenticated)
 
-app.use('/', routes);
+app.use('/', routes)
 app.get('/login', (req, res) => {
-  res.render('login', { message: req.session.messages || '' });
-});
+  res.render('login', { message: req.session.messages || '' })
+})
 
 app.post('/login',
   passport.authenticate('local', {
@@ -76,29 +76,29 @@ app.post('/login',
     failureRedirect: '/login',
     failureMessage: true,
   })
-);
+)
 
 // Logout route
 app.get('/logout', (req, res) => {
   req.logout(() => {
-    res.redirect('/login');
-  });
-});
+    res.redirect('/login')
+  })
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
